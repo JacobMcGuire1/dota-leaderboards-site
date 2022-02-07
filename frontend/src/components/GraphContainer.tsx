2a543f8b2284d23be7e717eff41961afb69bc779
Jacob McKinnon-Mcguire <mcguirejacob1@gmail.com>
2022-02-07 00:26:28 +0000

    Began adding UI options for data choice

--- frontend/src/components/GraphContainer.tsx
+++ frontend/src/components/GraphContainer.tsx
@@ -5,6 +5,7 @@ import '../App.css';
 import {getTopPlayers, decodeData, getPlayerList} from '../data/accessapi'
 import Graph from './Graph'
 import { getTextOfJSDocComment } from 'typescript';
+import { AnyRecord } from 'dns';
 
 enum APIMode {
     PlayerList,
@@ -47,9 +48,61 @@ class GraphContainer extends React.Component<{}, State> {
                 return this.getTopNRanks();
         }
     }
+    private submitOptionsForm() {
+        
+    }
+    private onValueChange(event: any, stateproperty: string) {
+        // @ts-ignore
+        this.setState({
+            [stateproperty]: event.target.value
+        });
+    }
+    private generateRadial(stateproperty: string, value: any, displayname: string) {
+        return (
+            
+            <label>
+                    <input type="radio" value={value} checked={
+                        // @ts-ignore
+                        this.state[stateproperty as any] === value} onChange={(event: any) => this.onValueChange(event, stateproperty)}/>
+                    {displayname}
+            </label>
+        );
+    }
+    private getOptionsForm() {
+        return (
+            <form onSubmit={this.submitOptionsForm}>
+                {this.generateRadial("region", Region.Europe, "Europe")}
+                {this.generateRadial("region", Region.Americas, "Americas")}
+                {this.generateRadial("region", Region.SEA, "South East Asia")}
+                {this.generateRadial("region", Region.China, "China")}
+                {/*<label>
+                    <input type="radio" value={Region.Americas} checked={this.state.region === Region.Americas} onChange={(event) => this.onValueChange(event)}/>
+                    Americas
+                </label>
+                <label>
+                    <input type="radio" value={Region.Europe} checked={this.state.region === Region.Europe} onChange={(event) => this.onValueChange(event)}/>
+                    Europe
+                </label>
+                <label>
+                    <input type="radio" value={Region.SEA} checked={this.state.region === Region.SEA} onChange={(event) => this.onValueChange(event)}/>
+                    SE Asia
+                </label>
+                <label>
+                    <input type="radio" value={Region.China} checked={this.state.region === Region.China} onChange={(event) => this.onValueChange(event)}/>
+                    China
+                </label>
+                <button>
+                Submit
+                </button>*/}
+            </form>
+        );
+    }
     public render() {
         return (
-            <Graph dataset={[]} datapromise={this.getGraphData()} showdatalist={false}/>
+            <div>
+                <Graph dataset={[]} datapromise={this.getGraphData()} showdatalist={false}/>
+                {this.getOptionsForm()}
+            </div>
         );
     }
 }
